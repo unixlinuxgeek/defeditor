3c2d569e89a21203a3f5c0964c272e5c1ea1b06e
unixlinuxgeek <unixlinuxgeek@gmail.com>
2024-09-13 19:26:08 +0500

    updated tests

--- defeditor_test.go
+++ defeditor_test.go
@@ -2,13 +2,12 @@ package defeditor
 
 import (
 	"math/rand/v2"
-	"strconv"
 	"testing"
 )
 
 func TestOpen(t *testing.T) {
 	n := rand.Int()
-	s, err := Open("tmp_" + strconv.Itoa(n) + ".txt")
+	s, err := Open()
 	if err != nil {
 		t.Errorf("open_editor: %s\n", err)
 	}
