93803993e67b2b53dd95f56558d4d2af4893c699
unixlinuxgeek <unixlinuxgeek@gmail.com>
2024-09-13 19:27:40 +0500

    fix tests

--- defeditor_test.go
+++ defeditor_test.go
@@ -1,12 +1,10 @@
 package defeditor
 
 import (
-	"math/rand/v2"
 	"testing"
 )
 
 func TestOpen(t *testing.T) {
-	n := rand.Int()
 	s, err := Open()
 	if err != nil {
 		t.Errorf("open_editor: %s\n", err)
